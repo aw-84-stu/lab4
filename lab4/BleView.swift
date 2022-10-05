0e9e400f44682e61feb178a6bffcc75e25fafba0
aw-stu-84 <s131104441@gmail.com>
2022-10-05 10:28:22 +0800

    2

--- lab4/BleView.swift
+++ lab4/BleView.swift
@@ -45,6 +45,10 @@ class BleClient: NSObject, ObservableObject {
     @Published var heartRateMeasurement: UInt16 = 0
     @Published var bodySensorLocation: String = "Unknown"
     var centralManager: CBCentralManager?
+    var heartRateMonitor: CBPeripheral?
+    var heartRateMeasurementCharacteristic: CBCharacteristic?
+    var bodySensorLocationCharacteristic: CBCharacteristic?
+    var heartRateControlPointCharacteristic: CBCharacteristic?
     
     override init() {
         super.init()
@@ -52,17 +56,35 @@ class BleClient: NSObject, ObservableObject {
     }
     
     func startScanning() {
+        centralManager?.scanForPeripherals(withServices: [CBUUID.heartRateServiceUUID], options: nil)
     }
     
     func registerHeartRateMeasurement() {
+        guard let characteristic = heartRateMeasurementCharacteristic else { return }
+        heartRateMonitor?.setNotifyValue(true, for: characteristic)
     }
-    
+
     func readBodySensorLocation() {
+        guard let characteristic = bodySensorLocationCharacteristic else { return }
+        heartRateMonitor?.readValue(for: characteristic)
+    }
+
+    func writeHeartRateControlPoint(_ controlPoint: Data) {
+        guard let characteristic = heartRateControlPointCharacteristic else { return }
+        heartRateMonitor?.writeValue(controlPoint, for: characteristic, type: .withResponse)
     }
     
-    func writeHeartRateControlPoint(_ controlPoint: Data) {
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber){
+        
+        print(peripheral);
+        status = "Discovered peripheral: \(peripheral)"
+        
+        heartRateMonitor = peripheral
+        centralManager?.connect(peripheral, options: nil)
     }
+    
 }
+
 
 
 extension BleClient: CBCentralManagerDelegate {
@@ -70,6 +92,57 @@ extension BleClient: CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         status = central.state.stringValue
     }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+        print(peripheral)
+        status = "Connected peripheral: \(peripheral)"
+        
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+}
+
+extension BleClient: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        guard let services = peripheral.services else { return }
+        
+        print(services)
+        status = "Discovered services: \(services)"
+        
+        for service in services {
+            
+            if (service.uuid == CBUUID.heartRateServiceUUID) {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+            
+        guard let characteristics = service.characteristics else { return }
+        
+        print(characteristics)
+        status = "Discovered Characteristics: \(characteristics)"
+        
+        for characteristic in characteristics {
+            
+            if (characteristic.uuid == CBUUID.heartRateMeasurementUUID) {
+                heartRateMeasurementCharacteristic = characteristic
+            }
+            
+            if (characteristic.uuid == CBUUID.bodySensorLocationUUID) {
+                bodySensorLocationCharacteristic = characteristic
+            }
+            
+            if (characteristic.uuid == CBUUID.heartRateControlPointUUID) {
+                heartRateControlPointCharacteristic = characteristic
+            }
+        }
+    }
+    
 }
 
 extension CBManagerState {
@@ -90,6 +163,33 @@ extension CBManagerState {
             return "This device doesn’t support the Bluetooth low energy central or client role."
         @unknown default:
             return "State is unknown."
+        }
+    }
+}
+
+extension CBUUID {
+    
+    static public let heartRateServiceUUID = CBUUID(string: "180D")
+    static public let heartRateMeasurementUUID = CBUUID(string: "2A37")
+    static public let bodySensorLocationUUID = CBUUID(string: "2A38")
+    static public let heartRateControlPointUUID = CBUUID(string: "2A39")
+}
+
+extension Data {
+    var bodySensorLocation: String {
+        
+        guard let byte = self.first else { return "Error" }
+        
+        switch byte {
+        case 0: return "Other"
+        case 1: return "Chest"
+        case 2: return "Wrist"
+        case 3: return "Finger"
+        case 4: return "Hand"
+        case 5: return "Ear Lobe"
+        case 6: return "Foot"
+        default:
+            return "Reserved for future use"
         }
     }
 }
